50d5905d07354611d9ef52cd86ceb696877f804e
Manuelomar <zaimon321@gmail.com>
2026-06-29 20:39:33 -0400

    feat: implement serverless handler for NestJS application in api/index.ts

--- api/index.ts
+++ api/index.ts
@@ -8,38 +8,40 @@ import { TransformInterceptor } from '../src/common/interceptors/transform.inter
 import { AllExceptionsFilter } from '../src/common/filters/http-exception.filter';
 import express from 'express';
 
-const server = express();
+let app: any;
 
-export const createNestServer = async (expressInstance: express.Express) => {
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(expressInstance),
-  );
+export default async function handler(req: any, res: any) {
+  if (!app) {
+    console.log('[NestJS] Inicializando servidor Serverless...');
+    const expressInstance = express();
+    app = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressInstance),
+    );
 
-  app.enableCors({
-    origin: true,
-    credentials: true,
-  });
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  app.useGlobalInterceptors(new TransformInterceptor());
-  app.useGlobalFilters(new AllExceptionsFilter());
+    app.enableCors({
+      origin: true,
+      credentials: true,
+    });
+    app.setGlobalPrefix('api');
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+    app.useGlobalInterceptors(new TransformInterceptor());
+    app.useGlobalFilters(new AllExceptionsFilter());
 
-  const config = new DocumentBuilder()
-    .setTitle('Pasteleria API')
-    .setDescription('The Bizcochao Pasteleria API description')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
+    const config = new DocumentBuilder()
+      .setTitle('Pasteleria API')
+      .setDescription('The Bizcochao Pasteleria API description')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+    
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/docs', app, document);
+
+    await app.init();
+    console.log('[NestJS] Servidor inicializado con éxito.');
+  }
   
-  // Swagger will be served at /api/docs (Vercel routes everything under api folder as /api/...)
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
-
-  await app.init();
-  return app;
-};
-
-// Start the Nest application
-createNestServer(server).then(() => console.log('Nest Ready on Vercel'));
-
-export default server;
+  const instance = app.getHttpAdapter().getInstance();
+  return instance(req, res);
+}
